a452dc0034761679be270bde7ef5c718f7c7fd4c
ShelbyBergin <shelbyjb23@gmail.com>
2024-01-14 19:43:12 -0600

    Chapter 7 Exercises: Strings

--- hello.js
+++ hello.js
@@ -1,8 +1,47 @@
-let num = 7;
+let num = 1001
 
-if (num % 2 === 0) {
-    console.log("even")
-    if (num % 2 === 1) {
-        console.log("odd");
-    }
+console.log(String(num).length)
+
+let numb = 123.45
+let nump = 12345
+
+console.log(String(numb).length)
+console.log(String(numb).replace('.','').length)
+
+if (String(numb).includes('.')) {
+    console.log(String(nump).length-1)
+} else {
+    console.log(String(nump).length)
 }
+
+let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT "
+dna = dna.trim().toUpperCase()
+console.log(dna)
+let dnaLength = dna.length
+
+console.log(dna.replace('GCT', 'AGG'))
+
+if (dna.indexOf("CAT")) {
+    console.log("CAT found")
+} else {
+    console.log("CAT NOT found")
+}
+console.log(dna.slice(16,19))
+
+console.log(`The DNA strand is ${dnaLength} characters long`)
+
+let jayS = "JavaScript"
+console.log(jayS.slice(0,1) + jayS.slice(4,5))
+
+//Without using slice(), use method chaining to accomplish the same thing.
+
+let lang = "JavaScript"
+let abbrev = "JS"
+
+console.log(`The abbreviation for "${lang}" is "${abbrev}"`)
+
+
+let titleCase = "title case"
+
+console.log(titleCase.replace('t', 'T').replace('c', 'C'))
+
